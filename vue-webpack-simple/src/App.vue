dab5f1ce9d4b39e4423d985ed6f2398129463113
Fernando Moreira <nandomoreira.me@gmail.com>
2017-07-02 10:17:32 -0300

    vue app

--- vue-webpack-simple/src/App.vue
+++ vue-webpack-simple/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1></h1>
+    <h1>{{msg}}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -30,31 +30,29 @@ export default {
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2E4053
+  margin-top: 60px
 
-h1, h2 {
-  font-weight: normal;
-}
+h1, h2
+  font-weight: normal
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+ul
+  list-style-type: none
+  padding: 0
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+li
+  display: inline-block
+  margin: 0 10px
 
-a {
-  color: #42b983;
-}
+a
+  color: #39AF78
+
+  &:hover
+    text-decoration: none
 </style>
